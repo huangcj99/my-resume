c50d8f15e16eaf26e2dd5f14b5b488c65b502776
smallcatcat <smallcatcat.joe@gmail.com>
2017-03-30 17:11:03 +0800

    first-commit my-resume

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom';
+import from 'rea'
+//引入样式
+import './common_style/reset.scss';
+
+//引入组建
+import ResumeCss from './components/ResumeCss/resumeCss.jsx';
+import ResumeHtml from './components/ResumeHtml/resumeHtml.jsx';
+
+
+ReactDOM.render(
+    (
+
+    )
+    document.getElementById('container')
+);
--- src/redux/states/states.jsx
+++
@@ -1,6 +0,0 @@
-let states = {
-    headerTitle:"主页",
-    maskLayer:true
-};
-
-export default states;
