66003fe73cb58f4822f7d61f7928b32c5d2773d7
smallcatcat <smallcatcat.joe@gmail.com>
2017-04-04 16:53:59 +0800

    delete koa project file

--- model/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by gunjoe on 2017/2/28.
- */
-let User = require('./account_model/user');
-
-module.exports = {
-    User:User
-};
--- test/master.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by gunjoe on 2017/2/27.
- */
-let fork = require('child_process').fork;
-let cpus = require('os').cpus();
-
-let n = fork('./process.js');
-
-console.log(__dirname);
